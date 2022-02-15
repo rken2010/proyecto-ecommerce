bc17fe78b5a1d942e9c01c0ec0535020980e6ac9
rken2010 <90514981+rken2010@users.noreply.github.com>
2022-02-15 20:22:47 -0300

    Counter (#3)
    * feat(counter): Se agrega contador de productos
    
    * fix(counter): Arreglo de importaciones y cambio de nombre al componente
    
    * feat(counter): Se agrego boton agregar al carrito y se mejoro el diseño- Falta arreglar la visualización de count en consola
    
    * fix(count): Se muestra la cantidad en consola

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+import { Button, Stack, ButtonGroup, Text, Heading } from '@chakra-ui/react'
+
+
+
+export const ItemCount = ({stock , initial, onAdd}) => {
+    const [count, setCount] = useState(parseInt(initial));
+    
+    const decrement = () => {
+        if (count > 0){setCount(count -1);}
+        else{ console.log("Error")}
+         }
+    const increment = () => {
+        if(stock > count) {setCount(count + 1);}
+        else{console.log("no hay stock")} 
+        }
+    function onAdd() { 
+        console.log ({count});
+    }
+    
+    return(
+        <>
+        <Stack alignItems="center" justifyContent="center" spacing={2}>
+                <Heading>Contador</Heading>
+                <Text fontSize='3xl'>{count}</Text>
+                <ButtonGroup size='sm' isAttached variant='outline'>
+                        <Button mr='-px' onClick={increment}>+</Button>
+                        <Button mr='-px' onClick={decrement}>-</Button>
+                </ButtonGroup>
+                <Button onClick={onAdd}>Agregar al Carrito</Button>
+        </Stack>
+        </>
+    );
+ }
