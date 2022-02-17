74290fc8dc54dc8deb59d10a4dc265c3501527e0
rken2010 <rkenshin.2010@gmail.com>
2022-02-17 19:50:30 -0300

    fix(item): Se paso onAdd como prop al component e

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,6 +1,13 @@
 import { Heading, Image, Stack, Box, Text, StackDivider } from "@chakra-ui/react";
+import { ItemCount } from "../ItemCount/ItemCount";
 
-const Item = ({ nombre, imagen, descripcion }) => {
+const Item = ({ nombre, imagen, descripcion, stock }) => {
+
+    function onAdd(count) { 
+
+        console.log ({count});
+    }
+
     return (
         <Stack spacing={2} alignItems="center" justifyContent="center" wrap="flexWrap" divider={<StackDivider borderColor='gray.200' />} borderRadius="lg" padding="15px" shadow="md">
            <Box>
@@ -10,7 +17,8 @@ const Item = ({ nombre, imagen, descripcion }) => {
            </Box>
            <Box>
            <Text fontSize='md' textAlign="center">{descripcion}</Text>
-           </Box>      
+           </Box>  
+           <ItemCount stock={stock} onAdd initial="1"/>    
         </Stack>
     );
 };
--- src/mock/catalogo.js
+++ src/mock/catalogo.js
@@ -1,8 +1,8 @@
 export const catalogo = [
-    { id:1, nombre:"camisa", imagen:"./img/camisa.jpg", descripcion:"lorem"},
-    { id:2, nombre:"Pantalon", imagen:"./img/camisa.jpg", descripcion:"lorem"},
-    { id:3, nombre:"zapatos", imagen:"./img/camisa.jpg", descripcion:"lorem"},
-    { id:4, nombre:"zapatillas", imagen:"./img/camisa.jpg", descripcion:"lorem"}
+    { id:1, nombre:"camisa", imagen:"./img/camisa.jpg", descripcion:"lorem", stock:"10"},
+    { id:2, nombre:"Pantalon", imagen:"./img/camisa.jpg", descripcion:"lorem", stock:"50"},
+    { id:3, nombre:"zapatos", imagen:"./img/camisa.jpg", descripcion:"lorem", stock:"20"},
+    { id:4, nombre:"zapatillas", imagen:"./img/camisa.jpg", descripcion:"lorem", stock:"5"}
 ]
 
 export const getProducts = new Promise((resolve, reject) => {
